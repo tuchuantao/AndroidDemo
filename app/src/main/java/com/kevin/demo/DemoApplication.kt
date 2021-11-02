9e09a2ad28a30645df51a95983483b2ca15897f0
tuchuantao <tuchuantao@kuaishou.com>
2021-11-02 11:23:42 +0800

    upgrade LeakCanary

--- app/src/main/java/com/kevin/demo/DemoApplication.kt
+++ app/src/main/java/com/kevin/demo/DemoApplication.kt
@@ -2,7 +2,6 @@ package com.kevin.demo
 
 import android.app.Application
 import androidx.multidex.MultiDex
-import com.squareup.leakcanary.LeakCanary
 
 /**
  * Create by Kevin-Tu on 2020/4/1.
@@ -12,13 +11,5 @@ class DemoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         MultiDex.install(this)
-
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-
     }
 }
