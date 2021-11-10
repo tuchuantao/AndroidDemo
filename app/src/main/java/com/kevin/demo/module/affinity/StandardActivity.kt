566a26f4927ea411c5ab89197325a6e87a6aa25a
tuchuantao <tuchuantao@kuaishou.com>
2021-11-10 16:53:46 +0800

    获取启动当前Activity的进程包名 和 上一个Activity ComponentName

--- app/src/main/java/com/kevin/demo/module/affinity/StandardActivity.kt
+++ app/src/main/java/com/kevin/demo/module/affinity/StandardActivity.kt
@@ -1,8 +1,16 @@
 package com.kevin.demo.module.affinity
 
+import android.os.Bundle
+
 
 /**
  * Create by Kevin-Tu on 2020/5/29.
  */
 class StandardActivity: BaseLaunchModeActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 必须使用startActivityForResult，否则获取不到
+        log("callingPackage=$callingPackage   callingActivity=$callingActivity")
+    }
 }
