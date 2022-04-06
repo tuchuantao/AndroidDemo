5ea2b114a62874e471cda6604b44c78e7983ba97
tuchuantao <tuchuantao@kuaishou.com>
2022-04-06 16:02:10 +0800

    原生接入Flutter

--- app/src/main/java/com/kevin/demo/MainActivity.kt
+++ app/src/main/java/com/kevin/demo/MainActivity.kt
@@ -39,208 +39,224 @@ import com.kevin.demo.module.tint.DrawableTintActivity
 import com.kevin.demo.module.touch.TouchEventActivity
 import com.kevin.demo.module.view.ViewMeasureActivity
 import com.kevin.demo.module.view.ViewTraversalActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
-    override fun getLayoutResId(): Int {
-        return R.layout.activity_main
+  override fun getLayoutResId(): Int {
+    return R.layout.activity_main
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    updateFontScale(1.3f)
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun initView(savedInstanceState: Bundle?) {
+    binding.btnFlutter.setOnClickListener {
+      var intent = FlutterActivity.createDefaultIntent(this)
+//      var intent = FlutterActivity.withNewEngine()
+//        // 背景颜色，透明 or 不透明
+//        .backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
+//        // 指定跳转的路由 home 页面
+//        .initialRoute("home")
+//        .build(this)
+      startActivity(intent)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        updateFontScale(1.3f)
-        super.onCreate(savedInstanceState)
+    binding.btnDatastore.setOnClickListener {
+      var intent = Intent(this, DataStoreActivity::class.java)
+      startActivity(intent)
+    }
+    binding.btnMmkv.setOnClickListener {
+      var intent = Intent(this, MMKVActivity::class.java)
+      startActivity(intent)
     }
 
-    override fun initView(savedInstanceState: Bundle?) {
-        binding.btnDatastore.setOnClickListener {
-            var intent = Intent(this, DataStoreActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnMmkv.setOnClickListener {
-            var intent = Intent(this, MMKVActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnScreenShoot.setOnClickListener {
+      var intent = Intent(this, ScreenShootActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnScreenShoot.setOnClickListener {
-            var intent = Intent(this, ScreenShootActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnParcel.setOnClickListener {
+      var intent = Intent(this, ParcelableActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnParcel.setOnClickListener {
-            var intent = Intent(this, ParcelableActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnBitmap.setOnClickListener {
+      var intent = Intent(this, BitmapActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnBitmap.setOnClickListener {
-            var intent = Intent(this, BitmapActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnViewTraversal.setOnClickListener {
+      var intent = Intent(this, ViewTraversalActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnViewTraversal.setOnClickListener {
-            var intent = Intent(this, ViewTraversalActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnViewMeasure.setOnClickListener {
+      var intent = Intent(this, ViewMeasureActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnViewMeasure.setOnClickListener {
-            var intent = Intent(this, ViewMeasureActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnTouchEvent.setOnClickListener {
+      var intent = Intent(this, TouchEventActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnTouchEvent.setOnClickListener {
-            var intent = Intent(this, TouchEventActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnLayoutInflater.setOnClickListener {
+      var intent = Intent(this, LayoutInflaterActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnLayoutInflater.setOnClickListener {
-            var intent = Intent(this, LayoutInflaterActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnFps.setOnClickListener {
+      var intent = Intent(this, FpsMonitorActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnFps.setOnClickListener {
-            var intent = Intent(this, FpsMonitorActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnHandler.setOnClickListener {
+      var intent = Intent(this, HandlerActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnHandler.setOnClickListener {
-            var intent = Intent(this, HandlerActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.btnToast.setOnClickListener {
+    binding.btnToast.setOnClickListener {
 //            Toast.makeText(this, "toast", Toast.LENGTH_SHORT).show()
-            // 小米手机Toast会默认带上应用名
-            // 解决方案1
-            var toast = Toast.makeText(this, "", Toast.LENGTH_SHORT)
-            toast.setText("toast")
-            toast.show()
-            // https://mp.weixin.qq.com/s/B3ooLGa-uQK4pf9RrZvY5g
-        }
+      // 小米手机Toast会默认带上应用名
+      // 解决方案1
+      var toast = Toast.makeText(this, "", Toast.LENGTH_SHORT)
+      toast.setText("toast")
+      toast.show()
+      // https://mp.weixin.qq.com/s/B3ooLGa-uQK4pf9RrZvY5g
+    }
 
-        binding.btnCoordinatorLayout.setOnClickListener {
-            var intent = Intent(this, CoordinatorLayoutActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnCoordinatorLayout.setOnClickListener {
+      var intent = Intent(this, CoordinatorLayoutActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnBannerLayout.setOnClickListener {
-            var intent = Intent(this, BannerActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnBannerLayout.setOnClickListener {
+      var intent = Intent(this, BannerActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnSurfaceView.setOnClickListener {
-            var intent = Intent(this, SurfaceViewActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnSurfaceView.setOnClickListener {
+      var intent = Intent(this, SurfaceViewActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnAidl.setOnClickListener {
-            var intent = Intent(this, AidlActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnAidl.setOnClickListener {
+      var intent = Intent(this, AidlActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnNotification.setOnClickListener {
-            var intent = Intent(this, NotificationActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnNotification.setOnClickListener {
+      var intent = Intent(this, NotificationActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnTabLayout.setOnClickListener {
-            var intent = Intent(this, TabLayoutActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnTabLayout.setOnClickListener {
+      var intent = Intent(this, TabLayoutActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnDrawableTint.setOnClickListener {
-            var intent = Intent(this, DrawableTintActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnDrawableTint.setOnClickListener {
+      var intent = Intent(this, DrawableTintActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnService.setOnClickListener {
-            var intent = Intent(this, ServiceActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnService.setOnClickListener {
+      var intent = Intent(this, ServiceActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnTaskAffinity.setOnClickListener {
-            var intent = Intent(this, StandardActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnTaskAffinity.setOnClickListener {
+      var intent = Intent(this, StandardActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnFadingEdge.setOnClickListener {
-            var intent = Intent(this, FadingEdgeActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnFadingEdge.setOnClickListener {
+      var intent = Intent(this, FadingEdgeActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnDataBinding.setOnClickListener {
-            var intent = Intent(this, DataBindingActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnDataBinding.setOnClickListener {
+      var intent = Intent(this, DataBindingActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnShowDialog.setOnClickListener {
-            var dialog = AlertDialog.Builder(this)
-                .setTitle("Alert Dialog")
-                .create()
-            dialog.show()
-        }
+    binding.btnShowDialog.setOnClickListener {
+      var dialog = AlertDialog.Builder(this)
+        .setTitle("Alert Dialog")
+        .create()
+      dialog.show()
+    }
 
-        binding.btnLottie.setOnClickListener {
-            var intent = Intent(this, LottieActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnLottie.setOnClickListener {
+      var intent = Intent(this, LottieActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnDrawableAnim.setOnClickListener {
-            var intent = Intent(this, DrawableAnimActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnDrawableAnim.setOnClickListener {
+      var intent = Intent(this, DrawableAnimActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnCustomView.setOnClickListener {
-            var intent = Intent(this, CustomActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnCustomView.setOnClickListener {
+      var intent = Intent(this, CustomActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnConstraintLayout.setOnClickListener {
-            var intent = Intent(this, ConstraintLayoutActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnConstraintLayout.setOnClickListener {
+      var intent = Intent(this, ConstraintLayoutActivity::class.java)
+      startActivity(intent)
+    }
 
-        binding.btnOkio.setOnClickListener {
-            var intent = Intent(this, OkioActivity::class.java)
-            startActivity(intent)
-        }
+    binding.btnOkio.setOnClickListener {
+      var intent = Intent(this, OkioActivity::class.java)
+      startActivity(intent)
+    }
 
-        // 正式环境可以抽出一个工具类
-        var myActivityLauncher = registerForActivityResult(MyActivityResultContract(), ActivityResultCallback {
-            Toast.makeText(this, "结果为：$it", Toast.LENGTH_LONG).show()
-        })
-        var takePicture = registerForActivityResult(ActivityResultContracts.TakePicturePreview(), ActivityResultCallback {
+    // 正式环境可以抽出一个工具类
+    var myActivityLauncher =
+      registerForActivityResult(MyActivityResultContract(), ActivityResultCallback {
+        Toast.makeText(this, "结果为：$it", Toast.LENGTH_LONG).show()
+      })
+    var takePicture = registerForActivityResult(
+      ActivityResultContracts.TakePicturePreview(),
+      ActivityResultCallback {
 
-        })
-        binding.btnNewActResult.setOnClickListener {
+      })
+    binding.btnNewActResult.setOnClickListener {
 //            myActivityLauncher.launch("Hello World !!")
-            takePicture.launch(null)
-        }
-
-        binding.btnDb.setOnClickListener {
-            val intent = Intent(this, DBActivity::class.java)
-            startActivity(intent)
-        }
-
-        //WorkHelper.startWorker(this)
+      takePicture.launch(null)
     }
 
-    /**
-     * 通过更改scaledDensity来更改文字的大小，只对SP有用
-     *
-     * 0.85 小, 1 标准大小, 1.15 大，1.3 超大 ，1.45 特大
-     */
-    private fun updateFontScale(scale: Float = 1f) {
-        var configuration = resources.configuration
-        configuration.fontScale = scale
-
-        var metrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(metrics)
-        metrics.scaledDensity = configuration.fontScale * metrics.density
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            baseContext.resources.displayMetrics.scaledDensity = metrics.scaledDensity
-            baseContext.createConfigurationContext(configuration)
-        } else {
-            baseContext.resources.updateConfiguration(configuration, metrics)
-        }
+    binding.btnDb.setOnClickListener {
+      val intent = Intent(this, DBActivity::class.java)
+      startActivity(intent)
     }
+
+    //WorkHelper.startWorker(this)
+  }
+
+  /**
+   * 通过更改scaledDensity来更改文字的大小，只对SP有用
+   *
+   * 0.85 小, 1 标准大小, 1.15 大，1.3 超大 ，1.45 特大
+   */
+  private fun updateFontScale(scale: Float = 1f) {
+    var configuration = resources.configuration
+    configuration.fontScale = scale
+
+    var metrics = DisplayMetrics()
+    windowManager.defaultDisplay.getMetrics(metrics)
+    metrics.scaledDensity = configuration.fontScale * metrics.density
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+      baseContext.resources.displayMetrics.scaledDensity = metrics.scaledDensity
+      baseContext.createConfigurationContext(configuration)
+    } else {
+      baseContext.resources.updateConfiguration(configuration, metrics)
+    }
+  }
 }
